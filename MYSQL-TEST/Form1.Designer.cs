b745087b9c33f629e971a8b8f900b18f528e3df8
cagy520 <11018230@qq.com>
2018-09-05 11:03:24 +0800

    first

---
+++ MYSQL-TEST/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MYSQL_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbla = new System.Windows.Forms.Label();
+            this.lblb = new System.Windows.Forms.Label();
+            this.lblc = new System.Windows.Forms.Label();
+            this.lbld = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "写入VIP 172.16.108.110";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 155);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 449);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MSYQL-MASTER1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "奴隶1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MYSQL-MASTER2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(513, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "奴隶2";
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Location = new System.Drawing.Point(74, 125);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(41, 12);
+            this.lbla.TabIndex = 6;
+            this.lbla.Text = "label5";
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.Location = new System.Drawing.Point(221, 125);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(41, 12);
+            this.lblb.TabIndex = 7;
+            this.lblb.Text = "label6";
+            // 
+            // lblc
+            // 
+            this.lblc.AutoSize = true;
+            this.lblc.Location = new System.Drawing.Point(368, 125);
+            this.lblc.Name = "lblc";
+            this.lblc.Size = new System.Drawing.Size(41, 12);
+            this.lblc.TabIndex = 8;
+            this.lblc.Text = "label7";
+            // 
+            // lbld
+            // 
+            this.lbld.AutoSize = true;
+            this.lbld.Location = new System.Drawing.Point(515, 125);
+            this.lbld.Name = "lbld";
+            this.lbld.Size = new System.Drawing.Size(41, 12);
+            this.lbld.TabIndex = 9;
+            this.lbld.Text = "label8";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "停止写入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(456, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "1000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 616);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbld);
+            this.Controls.Add(this.lblc);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.lbla);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.Label lblc;
+        private System.Windows.Forms.Label lbld;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MYSQL-TEST/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace MYSQL_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread th;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(insert);
+            th.Start();
+            Thread th1 = new Thread(reada);
+            th1.Start();
+
+            Thread th2 = new Thread(readb);
+            th2.Start();
+
+            Thread th3 = new Thread(readc);
+            th3.Start();
+
+            Thread th4 = new Thread(readd);
+            th4.Start();
+        }
+        bool istop = true;
+        private void insert()
+        {
+            while (istop)
+            {
+                MySqlConnection sqlCnn = new MySqlConnection();
+                sqlCnn.ConnectionString = "Database=tx;Data Source=172.16.108.110;Port=3306;User Id=root;Password=1;Charset=utf8;";
+                MySqlCommand sqlCmd = new MySqlCommand();
+                sqlCmd.Connection = sqlCnn;
+                sqlCmd.CommandText = "INSERT INTO ttxx(NAME) VALUES('dfhsdjkhfjkasdhfjkhsdjk');INSERT INTO ttxx(NAME) VALUES('dfhsdjkhfjkasdhfjkhsdjk');INSERT INTO ttxx(NAME) VALUES('dfhsdjkhfjkasdhfjkhsdjk');INSERT INTO ttxx(NAME) VALUES('dfhsdjkhfjkasdhfjkhsdjk');";
+
+                try
+                {
+                    sqlCnn.Open();
+                    int row = sqlCmd.ExecuteNonQuery();
+                    sqlCnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += "插入失败:"+ex.Message+"\r\n";
+                    sqlCnn.Close();
+                }
+                Thread.Sleep(Convert.ToInt32(textBox2.Text));
+            }
+
+        }
+
+
+        private void reada()
+        {
+            while (true)
+            {
+                MySqlConnection sqlCnn = new MySqlConnection();
+                sqlCnn.ConnectionString = "Database=tx;Data Source=172.16.108.71;Port=3306;User Id=root;Password=1;Charset=utf8;";
+                MySqlCommand sqlCmd = new MySqlCommand();
+                sqlCmd.Connection = sqlCnn;
+                sqlCmd.CommandText = "select count(*) from ttxx;";
+
+                try
+                {
+                    sqlCnn.Open();
+                    int row = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    sqlCnn.Close();
+                    lbla.Text = row.ToString() + "条数据";
+                    label1.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += "读取71失败:" + ex.Message + "\r\n";
+                    label1.BackColor = Color.Red;
+                    sqlCnn.Close();
+                }
+                Thread.Sleep(50);
+            }
+        }
+        private void readb()
+        {
+            while (true)
+            {
+                MySqlConnection sqlCnn = new MySqlConnection();
+                sqlCnn.ConnectionString = "Database=tx;Data Source=172.16.108.73;Port=3306;User Id=root;Password=1;Charset=utf8;";
+                MySqlCommand sqlCmd = new MySqlCommand();
+                sqlCmd.Connection = sqlCnn;
+                sqlCmd.CommandText = "select count(*) from ttxx;";
+
+                try
+                {
+                    sqlCnn.Open();
+                    int row = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    sqlCnn.Close();
+                    lblb.Text = row.ToString() + "条数据";
+                    label2.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += "读取73失败:" + ex.Message + "\r\n";
+                    label2.BackColor = Color.Red;
+                    sqlCnn.Close();
+                }
+                Thread.Sleep(50);
+            }
+        }
+        private void readc()
+        {
+            while (true)
+            {
+                MySqlConnection sqlCnn = new MySqlConnection();
+                sqlCnn.ConnectionString = "Database=tx;Data Source=172.16.108.72;Port=3306;User Id=root;Password=1;Charset=utf8;";
+                MySqlCommand sqlCmd = new MySqlCommand();
+                sqlCmd.Connection = sqlCnn;
+                sqlCmd.CommandText = "select count(*) from ttxx;";
+
+                try
+                {
+                    sqlCnn.Open();
+                    int row = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    sqlCnn.Close();
+                    lblc.Text = row.ToString() + "条数据";
+                    label3.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += "读取72失败:" + ex.Message + "\r\n";
+                    label3.BackColor = Color.Red;
+                    sqlCnn.Close();
+                }
+                Thread.Sleep(50);
+            }
+        }
+        private void readd()
+        {
+            while (true)
+            {
+                MySqlConnection sqlCnn = new MySqlConnection();
+                sqlCnn.ConnectionString = "Database=tx;Data Source=172.16.108.92;Port=3306;User Id=root;Password=1;Charset=utf8;";
+                MySqlCommand sqlCmd = new MySqlCommand();
+                sqlCmd.Connection = sqlCnn;
+                sqlCmd.CommandText = "select count(*) from ttxx;";
+
+                try
+                {
+                    sqlCnn.Open();
+                    int row = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    sqlCnn.Close();
+                    lbld.Text = row.ToString() + "条数据";
+                    label4.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += "读取92失败:" + ex.Message + "\r\n";
+                    label4.BackColor = Color.Red;
+                    sqlCnn.Close();
+                }
+                Thread.Sleep(50);
+                if (textBox1.Text.Length > 1000) textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                istop = false;
+                th.Abort();
+            }
+            catch (Exception)
+            {
+
+                th.Abort();
+            }
+           
+        }
+    }
+}
